e08d1a5b036fed031eabb3ea503f69bfe36fdbcf
nicefull <madnadika@gmail.com>
2022-09-19 22:36:18 +0300

    last part

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const { email, password } = evt.currentTarget.elements;
+  if (email.value === '' || password.value === '') {
+    alert('Please, fiil all fields!!');
+  } else {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    evt.currentTarget.reset();
+    console.log(data);
+  }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('button');
+const textBgColor = document.querySelector('.color');
+const bgColorOfBody = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', onClickBtn);
+function onClickBtn() {
+  bgColorOfBody.style.backgroundColor = getRandomHexColor();
+  textBgColor.textContent = getRandomHexColor();
+}
